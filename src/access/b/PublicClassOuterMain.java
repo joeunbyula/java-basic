54f5cc5df11db60403ad4fe14c03a9bc1744c671
joeunbyula <gkdldy1109@gmail.com>
2023-12-15 14:40:34 +0900

    자바 메모리 구조, 스택/힙 영역, static 변수

--- src/access/b/PublicClassOuterMain.java
+++ src/access/b/PublicClassOuterMain.java
@@ -3,6 +3,8 @@ package access.b;
 
 import access.a.PublicClass;
 
+import java.util.Date;
+
 public class PublicClassOuterMain {
     public static void main(String[] args) {
         PublicClass publicClass = new PublicClass();
@@ -10,5 +12,6 @@ public class PublicClassOuterMain {
 //        DefaultClass1 defaultClass1 = new DefaultClass1();
 //        DefaultClass2 defaultClass1 = new DefaultClass2();
 
+        System.out.println(new Date());
     }
 }
