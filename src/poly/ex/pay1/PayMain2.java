4836e769e07d53e3e72d4044e1f9b25dc1eb31d2
joeunbyula <gkdldy1109@gmail.com>
2023-12-22 16:27:57 +0900

    OCP원칙 - 문제풀이

---
+++ src/poly/ex/pay1/PayMain2.java
@@ -0,0 +1,30 @@
+package poly.ex.pay1;
+
+import java.util.Scanner;
+
+public class PayMain2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("결제수단을 입력하세요. : ");
+            String payOption = scanner.nextLine();
+            if("exit".equals(payOption)) {
+                System.out.println("프로그램을 종료합니다.");
+                return;
+            }
+
+            System.out.print("금액을 입력하세요. : ");
+            int amount = scanner.nextInt();
+
+            scanner.nextLine();
+
+            PayService payService = new PayService();
+            payService.processPay(payOption, amount);
+        }
+
+    }
+
+}
